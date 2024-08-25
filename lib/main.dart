dd484576a6e3c1ea7df337e4c7de41268357cb73
PHANNA11 <phaphanna2018@gmail.com>
2024-08-25 16:11:05 +0700

    commit code

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:app_ui/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
